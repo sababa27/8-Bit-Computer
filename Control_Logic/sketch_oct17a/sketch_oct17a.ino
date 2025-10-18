50b93adfa5d078c5cee70f6aac73befdc262b5a7
teklala <tvada22@freeuni.edu.ge>
2025-10-18 20:25:27 +0400

    updated control logic code

--- Control_Logic/sketch_oct17a/sketch_oct17a.ino
+++ Control_Logic/sketch_oct17a/sketch_oct17a.ino
@@ -1,49 +1,143 @@
+uint16_t A_In = 0b0000000000000001;
+uint16_t A_Out = 0b0000000000000010;
+uint16_t B_In = 0b0000000000000100;
+uint16_t Flags_In = 0b0000000000001000;
+uint16_t Alu_Sub = 0b0000000000010000;
+uint16_t Alu_Out = 0b0000000000100000;
+uint16_t Counter_En = 0b0000000001000000;
+uint16_t Counter_Out = 0b0000000010000000;
+uint16_t Counter_Jump = 0b0000000100000000;
+uint16_t Instruction_In = 0b0000001000000000;
+uint16_t Instruction_Out = 0b0000010000000000;
+uint16_t Ram_Adr_In = 0b0000100000000000;
+uint16_t Ram_Data_In = 0b0001000000000000;
+uint16_t Ram_Data_Out = 0b0010000000000000;
+uint16_t Output_In = 0b0100000000000000;
+uint16_t Hlt = 0b1000000000000000;
+
 uint16_t fetch(byte micro){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b000)
+    ans = Counter_Out + Ram_Adr_In;
+
+  else if(micro == 0b001)
+    ans = Ram_Data_Out + Instruction_In + Counter_En;
+
+  return ans;
 }
 
 uint16_t NOP(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+  return ans;
 }
 
 uint16_t LDA(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Instruction_Out + Ram_Adr_In;
+  
+
+  else if(micro == 0b011)
+    ans = Ram_Data_Out + A_In;
+  
+  return ans;
 }
 
 uint16_t ADD(byte micro, byte flags){
-  return 0;
+  int16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Instruction_Out+Ram_Adr_In;
+
+  else if(micro == 0b011)
+    ans = Ram_Data_Out + B_In;
+
+  else if(micro == 0b100)
+    ans = Alu_Out + A_In + Flags_In;
+
+  return ans;
 }
 
 uint16_t SUB(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Instruction_Out + Ram_Adr_In;
+
+  else if(micro == 0b011)
+    ans = Ram_Data_Out + B_In;
+
+  else if(micro == 0b100)
+    ans = Alu_Sub + Alu_Out + A_In + Flags_In;
+
+  return ans;
 }
 
 uint16_t STA(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Instruction_Out + Ram_Adr_In;
+
+  else if(micro == 0b011)
+    ans = A_Out + Ram_Data_In;
+  return ans;
 }
 
 uint16_t LDI(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Instruction_Out + A_In;
+
+  return ans;
 }
 
 uint16_t JMP(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro = 0b010)
+    ans = Instruction_Out + Counter_Jump;
+
+  return ans;
 }
 
 uint16_t JMC(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(flags == 0b10 || flags == 0b11)
+    ans = Instruction_Out + Counter_Jump;
+
+  return ans;
 }
 
 uint16_t JMZ(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(flags == 0b01 || flags == 0b11)
+    ans = Instruction_Out + Counter_Jump;
+
+  return ans;
 }
 
 uint16_t OUT(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = A_Out + Output_In;
+
+  return ans;
 }
 
 uint16_t HLT(byte micro, byte flags){
-  return 0;
+  uint16_t ans = 0;
+
+  if(micro == 0b010)
+    ans = Hlt;
+
+  return ans;
 }
 
 
